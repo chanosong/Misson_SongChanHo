53ea02f74e3fbec5b5c8da601e0dcd2e3fbed945
chener <49267211+chanosong@users.noreply.github.com>
2023-05-01 21:41:44 +0900

    feat: notification 생성, 폰트 변경

---
+++ src/main/java/com/ll/gramgram/boundedContext/notification/entity/Notification.java
@@ -0,0 +1,35 @@
+package com.ll.gramgram.boundedContext.notification.entity;
+
+import com.ll.gramgram.base.baseEntity.BaseEntity;
+import com.ll.gramgram.boundedContext.instaMember.entity.InstaMember;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@SuperBuilder
+@ToString(callSuper = true)
+public class Notification extends BaseEntity {
+    private LocalDateTime readDate;
+
+    @ManyToOne
+    @ToString.Exclude
+    private InstaMember toInstaMember; // 호감을 받는 사람
+
+    @ManyToOne
+    @ToString.Exclude
+    private InstaMember fromInstaMember; // 호감을 보내는 사람
+
+    private String typeCode; // 호감표시일시 Like, 사유변경일시 ModifyAttractiveType
+    private String oldGender; // 해당사항 없을 시 null
+    private int oldAttributeTypeCode; // 해당사항 없을 시 0
+    private String newGender; // 해당사항 없을 시 null
+    private int newAttractiveTypeCode; // 해당사항 없을 시 0
+}
