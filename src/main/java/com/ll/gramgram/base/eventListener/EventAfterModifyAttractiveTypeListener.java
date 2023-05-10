39aeac93dad38380dca74a9359b8cec7a098188b
chener <49267211+chanosong@users.noreply.github.com>
2023-05-10 10:35:30 +0900

    feat: EventListener에 @Transcational 추가

--- src/main/java/com/ll/gramgram/base/eventListener/EventAfterModifyAttractiveTypeListener.java
+++ src/main/java/com/ll/gramgram/base/eventListener/EventAfterModifyAttractiveTypeListener.java
@@ -5,10 +5,12 @@ import com.ll.gramgram.boundedContext.notification.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 // 호감 사유 변경이 일어날 경우 Listener
 @RequiredArgsConstructor
 @Component
+@Transactional
 public class EventAfterModifyAttractiveTypeListener implements ApplicationListener<EventAfterModifyAttractiveType> {
     private final NotificationService notificationService;
 
